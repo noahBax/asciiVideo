c19a66e1c94da689af6baec1b43914374a248676
Noah Baxley <noah.baxley@gmail.com>
2022-05-09 14:44:44 -0500

    first commit

---
+++ ts/beaver.ts
@@ -0,0 +1,109 @@
+function beaver() {
+
+
+    const TEST_IMAGE = document.getElementById("image1") as HTMLImageElement;
+
+
+    const C1 = document.createElement("canvas") as HTMLCanvasElement;
+    const BASE_IMAGE = C1.getContext("2d");
+    const IMG_1 = new Image();
+    
+    IMG_1.src = TEST_IMAGE.src;
+    
+    console.log("Pixelating")
+    // TEST_IMAGE.remove();
+
+    let w = IMG_1.width;
+    let h = IMG_1.height;
+    
+    C1.width = w;
+    C1.height = h;
+    BASE_IMAGE.drawImage(IMG_1, 0, 0);
+
+    
+    
+    // Pixelate the image first
+    // const PIXELATED_CANVAS = document.createElement("canvas");
+    // PIXELATED_CANVAS.width = w;
+    // PIXELATED_CANVAS.height = h;
+    // const PIXELATED_CTX = PIXELATED_CANVAS.getContext("2d");
+    
+    let pixelArr = BASE_IMAGE.getImageData(0, 0, w, h).data;
+    
+    console.log("Here")
+    let maxBrightness = 0;
+
+    
+    
+    for (let y = 0; y < h; y += pixelSize) {
+        for (let x = 0; x < w; x += pixelSize) {
+            let p = (x + y * w) * 4;
+            const r = pixelArr[p];
+            const g = pixelArr[p + 1];
+            const b = pixelArr[p + 2];
+            const avg = ( r + g + b) / 3;
+
+            maxBrightness = (avg > maxBrightness) ? avg : maxBrightness;
+            
+            // PIXELATED_CTX.fillStyle = "rgba(" + pixelArr[p] + ", " + pixelArr[p + 1] + ", " + pixelArr[p + 2] + pixelArr[p + 3] + ")";
+            // PIXELATED_CTX.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+        }
+    }
+    
+    // const PIXELATED = new Image();
+    // PIXELATED.src = PIXELATED_CANVAS.toDataURL("image/jpeg");
+    // PIXELATED.width = 600;
+    // PIXELATED.id = "image2";
+    // document.body.appendChild(PIXELATED)
+    // console.log(PIXELATED);
+
+    console.log("Grayscaling")
+
+    // const GRAYSCALE_CANVAS = document.createElement("canvas") as HTMLCanvasElement;
+    // const GRAYSCALE_CTX = GRAYSCALE_CANVAS.getContext("2d");
+    // GRAYSCALE_CANVAS.width = w;
+    // GRAYSCALE_CANVAS.height = h;
+    // GRAYSCALE_CTX.font = "20px Arial";
+    
+
+    let docString = "";
+    
+    // Get the brightness values and fill those in
+    for (let y = 0; y < h; y += pixelSize) {
+        for (let x = 0; x < w; x += pixelSize) {
+            let p = (x + y * w) * 4;
+            const r = pixelArr[p];
+            const g = pixelArr[p + 1];
+            const b = pixelArr[p + 2];
+            const avg = ( r + g + b) / 3;
+
+            // console.log(avg)
+            
+            // GRAYSCALE_CTX.fillStyle = "rgb(" + avg + "," + avg + "," + avg + ")";
+            // GRAYSCALE_CTX.fillStyle = "white";
+            // ctx2.fillRect(x, y, PIXEL_SIZE, PIXEL_SIZE);
+            const INDEX = Math.floor(avg / maxBrightness * (DENSITY.length - 1))
+
+            // GRAYSCALE_CTX.fillText(DENSITY[INDEX], x, y, PIXEL_SIZE)
+
+            if (DENSITY[INDEX] == undefined) {
+                console.log(INDEX, DENSITY[INDEX])
+            }
+            
+            docString += (INDEX < 4) ? "\b" : DENSITY[INDEX];
+        }
+        docString += "<br />";
+    }
+    // const GRAYSCALE = new Image();
+    // GRAYSCALE.src = GRAYSCALE_CANVAS.toDataURL("image/jpeg");
+    // GRAYSCALE.width = 600;
+    // GRAYSCALE.id = "grayscale";
+    // document.body.appendChild(GRAYSCALE)
+    // console.log(GRAYSCALE);
+
+    const SPAN = document.createElement("span") as HTMLSpanElement;
+    SPAN.innerHTML = docString;
+    document.getElementById("beaver").appendChild(SPAN);
+}
+
+// beaver();
---
+++ ts/index.ts
@@ -0,0 +1,137 @@
+var pixelSize = 3;
+const DENSITY = ' .\'`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$'
+var chromaThreshold = 6923
+var brightnessThreshold = 1;
+var currentFrameData: {maxBrightness: number, lowestBrightness: number, indexString: number[][][]};
+function loaded() {
+    // document.getElementById("image1").addEventListener("load", beaver)
+
+    
+    document.getElementById("chroma").innerHTML = chromaThreshold + '';
+    document.getElementById("pixel").innerHTML = pixelSize + '';
+    document.getElementById('brightness').innerHTML = brightnessThreshold + "";
+    let densityString = "<mark>" + DENSITY.substring(0,brightnessThreshold) + "</mark>" + DENSITY.substring(brightnessThreshold);
+    document.getElementById("densitySlider").innerHTML = densityString;
+
+
+
+    document.getElementById("chromaSlider").addEventListener('input', (e) => {
+        let t = parseInt((e.target as HTMLInputElement).value);
+        chromaThreshold = t;
+        document.getElementById("chroma").innerHTML = t + '';
+        if (!playing && started) {
+            drawFrame(currentFrameData);
+        }
+    });
+
+    const R = document.querySelector(':root');
+    const SIZING_REF = [0,0,0,10,24,40]
+    // R.style.setProperty('--main-offset', SIZING_REF[brightnessThreshold] + 'px');
+
+    document.getElementById("pixelSlider").addEventListener('input', (e) => {
+        let t = parseInt((e.target as HTMLInputElement).value);
+        pixelSize = t;
+        document.getElementById("pixel").innerHTML = t + '';
+        R.style.setProperty('--main-offset', SIZING_REF[t] + 'px');
+        if (!playing && started) {
+            currentFrameData = createFrameData();
+            drawFrame(currentFrameData);
+        }
+    })
+
+    document.getElementById("brightSlider").addEventListener('input', (e) => {
+        let t = parseInt((e.target as HTMLInputElement).value);
+        brightnessThreshold = t;
+        document.getElementById('brightness').innerHTML = t + "";
+        let densityString = "<mark>" + DENSITY.substring(0,brightnessThreshold) + "</mark>" + DENSITY.substring(brightnessThreshold);
+        document.getElementById("densitySlider").innerHTML = densityString;
+        if (!playing && started) {
+            drawFrame(currentFrameData);
+        }
+    })
+
+    var started = false;
+    var playing = false;
+
+
+    // Main Meat
+    const VIDEO = document.getElementById("video1") as HTMLVideoElement;
+    const w = 1280;
+    const h = 720;
+    const VIDEO_CANVAS = document.createElement('canvas') as HTMLCanvasElement;
+        VIDEO_CANVAS.width = w;
+        VIDEO_CANVAS.height = h;
+    const ctx = VIDEO_CANVAS.getContext('2d');
+    const DIV = document.getElementById("display") as HTMLDivElement;
+    document.getElementById("zerotwo").appendChild(DIV);
+
+    let loop: number;
+
+    function drawFrame(frameData: {maxBrightness: number, lowestBrightness: number, indexString: number[][][]}) {
+        DIV.innerHTML = "";
+
+        let docString = "";
+
+        let range = frameData.maxBrightness - frameData.lowestBrightness;
+        for (let y = 0; y < frameData.indexString.length; y++) {
+            for (let x = 0; x < frameData.indexString[0].length; x++) {
+                if (frameData.indexString[y][x][1] < chromaThreshold) {
+                    docString += "&nbsp;";
+                } else {
+                    const INDEX = Math.floor((frameData.indexString[y][x][0] - frameData.lowestBrightness) / range * (DENSITY.length - 1));
+                    docString += (INDEX < brightnessThreshold) ? "&nbsp;" : DENSITY[INDEX];
+                }
+            }
+            docString += "<br />";
+        }
+
+        DIV.innerHTML = docString;
+    }
+
+    function createFrameData(): {maxBrightness: number, lowestBrightness: number, indexString: number[][][]} {
+        ctx.clearRect(0, 0, VIDEO.videoWidth, VIDEO.videoHeight);
+        ctx.drawImage(VIDEO, 0, 0, VIDEO.videoWidth, VIDEO.videoHeight);
+        
+        let pixelArr = ctx.getImageData(0, 0, w, h).data;
+
+        let DATA = {
+            maxBrightness: 0,
+            lowestBrightness: 255,
+            indexString: []
+        }
+        for (let y = 0; y < h / pixelSize; y += pixelSize) {
+            let build: number[][] = [];
+            for (let x = 0; x < w / pixelSize; x += pixelSize) {
+                let p = (x + y * w) * 4 * pixelSize;
+                const r = pixelArr[p];
+                const g = pixelArr[p + 1];
+                const b = pixelArr[p + 2];
+
+                const distance = (61-r)**2 + (51-g)**2 + (58-b)**2;
+                const avg = ( r + g + b) / 3;
+                build.push([avg, distance]);
+
+                DATA.maxBrightness = (avg > DATA.maxBrightness) ? avg : DATA.maxBrightness;
+                DATA.lowestBrightness = (avg < DATA.lowestBrightness) ? avg : DATA.lowestBrightness;
+            }
+            DATA.indexString.push(build);
+        }
+
+        return DATA;
+    }
+    
+    VIDEO.addEventListener('play', () => {
+        started = true;
+        playing = true;
+        loop = window.setInterval(() => {
+            currentFrameData = createFrameData();
+            drawFrame(currentFrameData);
+
+        }, 1000/60);
+    });
+    
+    VIDEO.addEventListener('pause', () => {
+        playing = false;
+        window.clearInterval(loop);
+    })
+}
